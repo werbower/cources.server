54212eded56bb504eef15493e936c11a0cd9452f
Name <werbower@gmail.com>
2017-03-01 17:35:03 +0200

    исходный под спринг

---
+++ softgroup/main/src/main/java/MainConfig.java
@@ -0,0 +1,5 @@
+/**
+ * Created by user on 01.03.2017.
+ */
+public class MainConfig {
+}
---
+++ softgroup/main/src/main/java/MainSpring.java
@@ -0,0 +1,8 @@
+/**
+ * Created by user on 01.03.2017.
+ */
+public class MainSpring {
+    public static void main(String[] args) {
+
+    }
+}
